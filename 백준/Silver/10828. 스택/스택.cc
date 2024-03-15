cfbbe5200bb84cd2ecfacef9c406e0174e1f9dcc
jiwoo420 <84079404+jiwoo420@users.noreply.github.com>
2024-03-15 15:21:52 +0900

    [Silver IV] Title: 스택, Time: 4 ms, Memory: 1500 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#define ll long long
+
+int stack[100000];
+int stack_size=0;
+void push(int push_data){
+    stack[stack_size] = push_data;
+    stack_size += 1;
+}
+
+int empty(){
+    if(stack_size == 0){
+        return 1;
+    }
+    return 0;
+}
+int pop(){
+    if(empty()){
+        return -1;
+    }
+    stack_size -= 1;
+    return stack[stack_size];
+}
+int top(){
+    if(empty()){
+        return -1;
+    }
+    return stack[stack_size-1];
+}
+int main(){
+
+    int a,b,k,i;
+    int N = 0, push_data = 0;
+    char command[5] = {0,};
+
+    scanf("%d",&N);
+
+    for(int i=0;i<N;i++){
+
+        scanf("%s",command);
+
+        if(!strcmp(command,"push")){
+            scanf("%d",&push_data);
+            push(push_data);
+        }
+        else if(!strcmp(command,"pop")){
+            printf("%d\n",pop());
+        }
+        else if(!strcmp(command,"empty")){
+            printf("%d\n",empty());
+        }
+        else if(!strcmp(command,"size")){
+            printf("%d\n",stack_size);
+        }
+        else if(!strcmp(command,"top")){
+            printf("%d\n",top());
+        }
+
+    }
+
+    return 0;
+    
+    
+}
